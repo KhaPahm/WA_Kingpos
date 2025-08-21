c79c7ad4c120b8f5c834bcefa1fcb5ee1362e2d7
KhaPahm <khapham.work@gmail.com>
2025-08-22 00:30:35 +0700

    kha update 2208

--- WA_Kingpos/wwwroot/js/views/nghiepvu/_ks_khachhang.js
+++ WA_Kingpos/wwwroot/js/views/nghiepvu/_ks_khachhang.js
@@ -21,8 +21,9 @@ let btnUsePhoto;
 let btnRetake;
 let btnTakePhoto;
 let btnSwitchCamera;
-let hiddenImage = document.getElementById('hiddenImage');
+let hiddenImage;
 
+//Lưu thông tin
 $('#btnSave').on('click', function () {
     var btn = $(this);
     btn.prop("disabled", true).text("Đang lưu...");
@@ -74,11 +75,14 @@ $('#btnSave').on('click', function () {
 });
 
 
-//Chụp ảnh
+//------------Start: Xử lý chụp ảnh---------------------
+
+//Dừng dùng camera camera
 function stopStream() {
     if (stream) { stream.getTracks().forEach(t => t.stop()); stream = null; }
 }
 
+//Mở camera
 async function startCamera() {
     // Stop any existing stream before starting a new one
     stopStream();
@@ -105,6 +109,7 @@ async function openCameraClick() {
     btnRetake = document.getElementById('btnRetake');
     btnTakePhoto = document.getElementById('btnTakePhoto');
     btnSwitchCamera = document.getElementById('btnSwitchCamera');
+    hiddenImage = document.getElementById('hiddenImage');
 
     try {
         await startCamera();
@@ -153,34 +158,66 @@ function reTakePhotoClick() {
     btnSwitchCamera.classList.remove('d-none');
 }
 
+async function switchCameraClick() {
+    currentFacing = currentFacing === 'user' ? 'environment' : 'user';
+    await startCamera();
+}
+
+function closeCameraModalClick() {
+    stopStream();
+    $('#cameraModal').modal('hide');
+}
+
 async function usePhotoClick() {
+
     const dataUrl = canvas.toDataURL('image/jpeg', 0.9);
     $('#cameraModal').modal('hide');
-    //$('#customerModal').modal('show');
     await processImage(dataUrl);
     stopStream();
 }
+//------------End: Xử lý chụp ảnh---------------------
 
+//------------Start: Xử lý upload ảnh---------------------
+async function fileInputChange(element) {
+    await handleImageInput(element);
+}
+
+async function handleImageInput(input) {
+    //if (input.files && input.files[0]) {
+    //    resizeImageTo300x300(input.files[0], (base64) => createImageReview(base64));
+    //}
+    const f = input.files && input.files[0];
+    if (f) await processFile(f);
+}
+
+async function processFile(file) {
+    if (!hiddenImage)
+        hiddenImage = document.getElementById('hiddenImage');
+    if (!hiddenBase64)
+        hiddenBase64 = document.getElementById('photoBase64');
+    const url = URL.createObjectURL(file);
+    await processImage(url);
+    URL.revokeObjectURL(url);
+}
+//------------End: Xử lý upload ảnh---------------------
+
+//------------Start: Xử lý cắt và vẽ ảnh lên canvas---------------------
 async function processImage(url) {
-    //var newPhoto = document.createElement('img');
-    //newPhoto.alt = "Customer photo";
-    //newPhoto.className = "img-thumbnail";
-    //newPhoto.style = "max-width:250px; max-height:250px; min-width:150px; min-height:150px";
-
     var $photoReview = $('#photoReview');
-    $photoReview.html('');
-    //$photoReview.append(newPhoto);
+    $photoReview.html('<button class="btn btn-outline-primary" type="button" disabled><span class="spinner-grow spinner-grow-sm text-primary" role="status" aria-hidden="true"></span> Đang kiểm tra ảnh...</button>');
 
-    return new Promise((url) => {
-        hiddenImage.onload = async (resolve) => {
+    return new Promise((resolve) => {
+        hiddenImage.onload = async () => {
             try {
-                const detections = await faceapi.detectAllFaces(img, new faceapi.TinyFaceDetectorOptions());
+                const detections = await faceapi.detectAllFaces(hiddenImage, new faceapi.TinyFaceDetectorOptions());
                 if (detections.length <= 0) {
+                    $photoReview.html('')
                     $photoReview.html('<div class="text-danger">KHÔNG PHÁT HIỆN KHUÔN MẶT TRONG ẢNH</div>');
                     resolve();
                     return;
                 }
-                else if (detections.length > 0) {
+                else if (detections.length > 1) {
+                    $photoReview.html('')
                     $photoReview.html('<div class="text-danger">PHÁT HIỆN NHIỀU HƠN 1 KHUÔN MẶT TRONG ẢNH</div>');
                     resolve();
                     return;
@@ -190,116 +227,43 @@ async function processImage(url) {
                 // Calculate a square crop centered on the face center
                 const centerX = box.x + box.width / 2;
                 const centerY = box.y + box.height / 2;
+                console.log("box.width: ", box.width);
+                console.log("box.height: ", box.height);
                 const cropSize = Math.max(box.width, box.height) * 1.6; // add padding so face is nicely centered
 
                 // Source rectangle
                 const sx = Math.max(0, Math.round(centerX - cropSize / 2));
-                const sy = Math.max(0, Math.round(centerY - cropSize / 2));
-                const sw = Math.min(cropSize, img.naturalWidth - sx);
-                const sh = Math.min(cropSize, img.naturalHeight - sy);
+                const sy = Math.max(0, Math.round(centerY - cropSize / 2 - 20));
+                const sw = Math.min(cropSize, hiddenImage.naturalWidth - sx);
+                const sh = Math.min(cropSize, hiddenImage.naturalHeight - sy);
 
-                const newCanvas = document.createElement('canvas');
+                const outCanvas = document.createElement('canvas');
+                outCanvas.classList.add("img-thumbnail");
                 const size = 300;
 
-                newCanvas.width = size;
-                newCanvas.height = size;
-                $photoReview.append(newCanvas);
+                outCanvas.width = size;
+                outCanvas.height = size;
+                $photoReview.html('')
+                $photoReview.append(outCanvas);
 
                 const outCtx = outCanvas.getContext('2d');
 
                 outCtx.clearRect(0, 0, outCanvas.width, outCanvas.height);
-                outCtx.drawImage(img, sx, sy, sw, sh, 0, 0, 300, 300);
+                outCtx.drawImage(hiddenImage, sx, sy, sw, sh, 0, 0, 300, 300);
+
+                const srcBase64 = outCanvas.toDataURL('image/jpeg', 0.9);
+                hiddenBase64.value = srcBase64.split(',')[1];
             } catch (err) {
                 console.error(err);
+                $photoReview.html('')
                 $photoReview.html('<div class="text-danger">Lỗi nhận diện khuôn mặt</div>');
             }
             resolve();
         };
-
-        hiddenImage.onerror = () => { alert('Could not load the image.'); resolve(); };
+        hiddenImage.onerror = () => { alert('Không thể load hình ảnh'); resolve(); };
         hiddenImage.src = url;
     });
 }
-
-//-------------------- Xử lý ảnh
-function resizeImageTo300x300(file, callback) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const img = new Image();
-        img.onload = function () {
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-            const size = 300;
-
-            canvas.width = size;
-            canvas.height = size;
-
-            // Fill with white background (optional, for JPG)
-            ctx.fillStyle = "#ffffff";
-            ctx.fillRect(0, 0, size, size);
-
-            // Draw image scaled to fill 300x300
-            ctx.drawImage(img, 0, 0, size, size);
-
-            const resizedDataUrl = canvas.toDataURL('image/jpeg', 0.9);
-            callback(resizedDataUrl);
-        };
-        img.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
-}
-
-async function dectionFace(img) {
-    
-    var $photoReview = $('#photoReview');
-    const detections = await faceapi.detectAllFaces(img, new faceapi.TinyFaceDetectorOptions());
-    if (detections.length == 1) {
-        document.getElementById('photoBase64').value = img.src.split(',')[1];
-    }
-    else if (detections.length <= 0) {
-        $photoReview.html('<div class="text-danger">KHÔNG PHÁT HIỆN KHUÔN MẶT TRONG ẢNH</div>');
-    }
-    else {
-        $photoReview.html('<div class="text-danger">PHÁT HIỆN NHIỀU HƠN 1 KHUÔN MẶT TRONG ẢNH</div>');
-    }
-}
-
-function createImageReview(src) {
-    var newPhoto = document.createElement('img');
-    newPhoto.alt = "Customer photo";
-    newPhoto.className = "img-thumbnail";
-    newPhoto.style = "max-width:250px; max-height:250px; min-width:150px; min-height:150px";
-    newPhoto.src = src;
-    //newPhoto.onload = async () => await dectionFace(newPhoto);
-    document.getElementById('photoBase64').value = src.split(',')[1];
-
-    var $photoReview = $('#photoReview');
-    $photoReview.html('');
-    $photoReview.append(newPhoto);
-}
-
-function handleImageInput(input) {
-
-    if (input.files && input.files[0]) {
-        resizeImageTo300x300(input.files[0], (base64) => createImageReview(base64));
-    }
-}
-
-function fileInputChange(element) {
-    handleImageInput(element);
-}
-
-async function switchCameraClick() {
-    currentFacing = currentFacing === 'user' ? 'environment' : 'user';
-    await startCamera();
-}
+//------------End: Xử lý cắt và vẽ ảnh lên canvas---------------------
 
 
-function closeCameraModalClick() {
-    $('#cameraModal').modal('hide');
-    //$('#customerModal').modal('show');
-}
-
-$('#cameraModal').on('hidden.bs.modal', function () {
-    if (stream) { stream.getTracks().forEach(t => t.stop()); stream = null; }
-});
